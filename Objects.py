6c8d7153d68bb638d8773bb2c3d415642f18cb71
Sampy <kazenovegoric@gmail.com>
2021-12-24 20:08:06 +0300

    Класс Объекта реализован
    Начата работа на классом игрока

--- Objects.py
+++ Objects.py
@@ -1,19 +1,38 @@
 import pygame
+
 pygame.init()
 
-class Object:
-    def __init__(self, spritepath, hitbox=('n', 'n'), coords=('n', 'n')):
-        self.sprite = pygame.image.load(spritepath)
-        if 'n' in hitbox + coords:
-            self.hitbox = self.sprite.get_rect()
-        else:
-            self.hitbox = pygame.rect.Rect(coords, hitbox)
-        self.layer = 0
+
+class Object(pygame.sprite.Sprite):  # Создание базового объекта
+    def __init__(self, path, coords, *args):
+        super().__init__(*args)
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = coords
+
+    def reform(self, w, h):  # Изменение размеров отгносительно размеров окна
+        self.rect.x *= w / 1920
+        self.rect.y *= h / 1080
+        self.rect.width *= w / 1920
+        self.rect.height *= h / 1080
+
+    def update(self, *args):
+        super().update(*args)
 
 
 # Объекты геймплея. Будут обрабатываться непосредственно во время игры
+
+
 class Player(Object):
-    pass
+    def __init__(self, path, coords, speed=10, *args):
+        super().__init__(path, coords, *args)
+        self.speed = speed
+
+    def update(self, events, m_pos, *args):
+        super().update(*args)
+        if pygame.KEYDOWN in events:
+            pass
+
 
 
 class SolidObj(Object):
@@ -28,6 +47,6 @@ class Item(Object):
     pass
 
 
- # В долгий ящик. От него будем наследовать хп-бары, диалоговые окна, инвентарь и т.п.
+# В долгий ящик. От него будем наследовать хп-бары, диалоговые окна, инвентарь и т.п.
 class UI(Object):
     pass
--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 import pygame
+from Objects import *
 
 pygame.init()
 settings = {}
+sprites_group = pygame.sprite.Group()
 
 with open("settings.txt", "r", encoding="utf-8") as f:
     for i in f.read().split("\n"):
         a, b = i.split(":")
-        settings[a] = b
+        settings[a] = b.split()
 
 # Здесь будет происходить выгрузка параметров из файла settings.txt
 size = list(map(int, settings["size"]))
 
-screen = pygame.display
-screen.set_mode(size)
+screen = pygame.display.set_mode(size)
 run = True
 
+obj = Object('textures/morgenshtern.jpg', (100, 100))
+obj.reform(size[0], size[1])
+sprites_group.add(obj)
+
 while run:
+    events = [event.type for event in pygame.event.get()]
+    m_pos = pygame.mouse.get_pos()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
             break
+    sprites_group.draw(screen)
+    sprites_group.update(events, m_pos)
     pygame.display.flip()
 
 pygame.quit()
